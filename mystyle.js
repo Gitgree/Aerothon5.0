cd5025e40be47dba55c02a16830076a25f474bfe
Gitgree <109200021+Gitgree@users.noreply.github.com>
2023-05-13 09:05:35 +0530

    Add files via upload

---
+++ mystyle.js
@@ -0,0 +1,6 @@
+submitBtn.addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent form submission
+    // Perform form validation or execute other actions
+    // ...
+  });
+
